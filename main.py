d8f624ebef285d94d2ae210160a436ab724972e1
ig-kh <ig.gr1nn1ng.kh@gmail.com>
2025-10-21 21:17:16 +0300

    2nd iter - formatting

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import pulp as pl
+from itertools import islice
+import os
+from argparse import ArgumentParser
+def load_data(inputs_path):
+
+
+
+def main():
+
+
+if __name__ == "__main__":
